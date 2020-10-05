445c4842c010e50c6868330ddd5f6cc613cd116a
juanr <juanr>
2020-10-05 12:49:02 -0700

    Initial commit for AddingMachine class, under cse360assignment02 project

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assignment02;
+/**
+ * 
+ * @author Juan Romero
+ * @version 1.0
+ *
+ */
+public class AddingMachine {
+	  private int total;
+	  /**
+	   * This is a constructor for the AddingMachine
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This method returns the total value of this AddingMachine
+	   * @return this adding machine's total
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * 
+	   * @param value Value to add to this AddingMachine's total
+	   */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * 
+	   * @param value Value to subtract from this AddingMachine's total
+	   */
+	  public void subtract (int value) {
+	  }
+	  
+	  /**
+	   * Currently returns an empty string
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method should clear this AddingMachine's total and reset it to 0
+	   */
+	  public void clear() {
+	  }
+	}
