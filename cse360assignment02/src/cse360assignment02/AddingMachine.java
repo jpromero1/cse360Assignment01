9e22bc5ee9cb3ac3799c2fbe94e93ec812049eaa
juanr <juanr@DESKTOP-UUURAAQ>
2020-10-05 13:17:14 -0700

    This updates and defines the methods, such as getTotal, add, subtract etc. It also adds necessary attributes to AddingMachine method to return appropriate operations as a string.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -5,13 +5,16 @@ package cse360assignment02;
  * @version 1.0
  *
  */
+
 public class AddingMachine {
 	  private int total;
+	  private String operations;
 	  /**
 	   * This is a constructor for the AddingMachine
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    operations = "0";
 	  }
 	  
 	  /**
@@ -19,33 +22,41 @@ public class AddingMachine {
 	   * @return this adding machine's total
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  /**
-	   * 
+	   * This method adds parameter value to this machine's total, and updates the operations string
+	   * for it to be printed in the toString method
 	   * @param value Value to add to this AddingMachine's total
 	   */
 	  public void add (int value) {
+		  total += value;
+		  operations += " + " + value;
 	  }
 	  
 	  /**
-	   * 
+	   * This method subtracts parameter value from this machine's total, and updates the operations string
+	   * for it to be printed in the toString method
 	   * @param value Value to subtract from this AddingMachine's total
 	   */
 	  public void subtract (int value) {
+		  total -= value;
+		  operations += " - " + value;
 	  }
 	  
 	  /**
-	   * Currently returns an empty string
+	   * This function returns the operations string that contains all operations performed by the adding machine
 	   */
 	  public String toString () {
-	    return "";
+	    return operations;
 	  }
 	  
 	  /**
-	   * This method should clear this AddingMachine's total and reset it to 0
+	   * This method clears this AddingMachine's total and operations string
 	   */
 	  public void clear() {
+		  operations = "0";
+		  total = 0;
 	  }
 	}
